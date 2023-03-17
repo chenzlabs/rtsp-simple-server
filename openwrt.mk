689f7d8da2995dad7048d4137f03a9eff88d1611
machenmusik <machenmusik@users.noreply.github.com>
2023-03-16 23:10:04 -0400

    Update openwrt.mk
    ```
    Finding targets
    flag provided but not defined: -buildvcs
    usage: go list [-f format] [-json] [-m] [list flags] [build flags] [packages]
    Run 'go help list' for details.
    
    Building targets
    flag provided but not defined: -buildvcs
    usage: go install [build flags] [packages]
    Run 'go help install' for details.
    ```

--- openwrt.mk
+++ openwrt.mk
@@ -5,20 +5,21 @@ PKG_VERSION:=v0.0.0
 PKG_RELEASE:=1
 
 PKG_SOURCE_PROTO:=git
-PKG_SOURCE_URL:=https://github.com/aler9/rtsp-simple-server
+PKG_SOURCE_URL:=https://github.com/chenzlabs/rtsp-simple-server
 PKG_SOURCE_VERSION:=$(PKG_VERSION)
 
 PKG_BUILD_DEPENDS:=golang/host
 PKG_BUILD_PARALLEL:=1
 PKG_USE_MIPS16:=0
 
-GO_PKG:=github.com/aler9/rtsp-simple-server
-GO_PKG_LDFLAGS_X:=github.com/aler9/rtsp-simple-server/internal/core.version=$(PKG_VERSION)
+GO_PKG:=github.com/chenzlabs/rtsp-simple-server
+GO_PKG_LDFLAGS_X:=github.com/chenzlabs/rtsp-simple-server/internal/core.version=$(PKG_VERSION)
 
 include $(INCLUDE_DIR)/package.mk
 include $(TOPDIR)/feeds/packages/lang/golang/golang-package.mk
 
-GO_MOD_ARGS:=-buildvcs=false
+#GO_MOD_ARGS:=-buildvcs=false
+GO_MOD_ARGS:=
 
 define Package/rtsp-simple-server
   SECTION:=net
